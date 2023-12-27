e823a937bb57eba7a44b79dd405288a2a37ad471
MYAIR <ymy0613@gmail.com>
2023-12-27 18:13:51 +0900

    수정 : mainpage 목업데이터 삭제

--- src/page/MainPage.tsx
+++ src/page/MainPage.tsx
@@ -1,10 +1,8 @@
 import { Box, Button, Container, Input } from "@mui/material";
 import { useTheme } from '@mui/material/styles'
 import { blue, red } from '@mui/material/colors'
-import { inherits } from "util";
 
 const MainPage = () => {
-  const theme = useTheme()
   return (
     <>
      <Container>
@@ -14,24 +12,6 @@ const MainPage = () => {
       }}>
         asdf
       </Box>
-      <Box sx={{ border : 3, minWidth: '100%',}}>ddd
-      </Box>
-      <Input name="인풋" type="text" sx={{
-        border: 2,
-        borderStyle: 'dashed',
-        borderColor: red[100],
-      }}/>
-      <Button sx={{
-        border: 5,
-        borderStyle: 'dotted',
-        borderColor: "coral",
-        color: 'inherit',
-      }}>
-        저장
-      </Button>
-      <Box sx={{ border : 3, borderColor: blue[400],minWidth: '100%',}}>111</Box>
-      <Box sx={{ border : 3,  minWidth: '100%',}}>222</Box>
-      <Box sx={{ border : 3, borderColor: blue[400],minWidth: '100%',}}>333</Box>
      </Container>
     </>
   );
