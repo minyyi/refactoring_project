9a2fb4ac4eaafe3cf92b7458a4df744450fc48ee
MYAIR <ymy0613@gmail.com>
2024-02-09 07:51:27 +0900

    추가: Reservation 라우터 추가

--- src/page/Reservation.tsx
+++ src/page/Reservation.tsx
@@ -1,10 +1,17 @@
+import PageContainer from '@/component/common/PageContainer';
+import Title from '@/component/common/Title';
 import { Box } from '@mui/material';
 
 const Reservation = () => {
   return (
-    <>
-      <Box>예약</Box>
-    </>
+    <PageContainer>
+      <Title>예약하기</Title>
+      <Box sx={{ display: 'flex' }}>
+        <Box>사진</Box>
+        <Box>달력</Box>
+        <Box>날짜</Box>
+      </Box>
+    </PageContainer>
   );
 };
 export default Reservation;
--- src/router.tsx
+++ src/router.tsx
@@ -3,6 +3,7 @@ import Home from './page/Home';
 import { Layout } from './component/layout/Layout';
 import LandingPage from './page/Landing';
 import Authpage from './page/Authpage';
+import Reservation from './page/Reservation';
 
 const Router = () => {
   return (
@@ -13,6 +14,7 @@ const Router = () => {
           <Route path="/login" element={<Authpage title={'로그인'} />} />
           <Route path="/signup" element={<Authpage title={'회원가입'} />} />
           <Route path="/home" element={<Home />} />
+          <Route path="/reservation" element={<Reservation />} />
         </Routes>
       </Layout>
     </BrowserRouter>
